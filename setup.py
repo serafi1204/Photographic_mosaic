e55fc8ff22d2fc39b5605afb89b63eab24bd9f60
serafi1204 <zlakalzn@gmail.com>
2025-09-09 23:36:30 +0900

    pip

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='photographic-mosaic',
+    version='0.1.0',
+    description='Photographic Mosaic',
+    author='serafi1204',
+    author_email='serafi1204@gmail.com',
+    url='https://github.com/serafi1204/Photographic_mosaic',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchvision',
+        'lpips',
+        'numpy',
+        'opencv-python',
+    ]
+)
