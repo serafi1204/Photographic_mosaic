f1aedaa0c1c4cc9ceb1df970283da61179d96c7a
serafi1204 <zlakalzn@gmail.com>
2025-09-20 17:45:44 +0900

    r20

--- photographic_mosaic/__init__.py
+++ photographic_mosaic/__init__.py
@@ -2,6 +2,6 @@ from .makeDataset import makeDataset
 from .makeMosaicMap import makeMosaicMap
 from .configuration import *
 from .resize import resize
-from .assamble import assamble
+from .assemble import assemble
 from .getYoutubeScreenshot import getYoutubeScreenshot
 from .getYoutubePlaylist import getYoutubePlaylist
